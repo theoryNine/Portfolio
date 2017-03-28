230ac3606b9577956e2b2c72398fca23d47a6f77
Ansel <anselcolvin@gmail.com>
2017-03-28 00:28:31 -0500

    Scaffold project

---
+++ Gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
+var minify = require('gulp-minify');
+
+gulp.task('copyPages', function() {
+  gulp.src('./src/**/*.html')
+  .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('minify-css', ['sass'], function() {
+  return gulp.src('./src/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('watch', ['minify-css'], function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./src/css/**/*.css', ['minify-css']);
+  gulp.watch('./src', ['copyPages']);
+});
+
+gulp.task('default', ['copyPages', 'sass', 'minify-css', 'watch']);
